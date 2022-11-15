9e0c7ff89c65c6b00da5ed16d420002926c67979
Aaron Latham <alatham7891@gmail.com>
2022-11-15 13:56:45 +0000

    Edited about section

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -2,19 +2,31 @@ import Card from "../components/shared/Card";
 import { Link } from "react-router-dom";
 
 function AboutPage() {
-  return (
-    <Card>
-      <div className="about">
-        <h1>About This Project</h1>
-        <p>This is a React app to leave feedback for a product or service.</p>
-        <p>Version: 1.0.0</p>
+   return (
+      <Card>
+         <div className="about">
+            <h1>About This Project</h1>
+            <p>
+               This is a React app to leave feedback for a product or service.
+            </p>
+            <p>
+               It requires json-server as a mock backend to function, clone and
+               install all packages to test locally
+            </p>
+            <p>
+               Github repo:{" "}
+               <a href="https://github.com/ddastardly91/feedback-app">
+                  https://github.com/ddastardly91/feedback-app
+               </a>
+            </p>
+            <p>Version: 1.0.0</p>
 
-        <p>
-          <Link to="/">Back to Homepage</Link>
-        </p>
-      </div>
-    </Card>
-  );
+            <p>
+               <Link to="/">Back to Homepage</Link>
+            </p>
+         </div>
+      </Card>
+   );
 }
 
 export default AboutPage;
